f620e96363835376b7a0419efb6deeaf3c788a9c
Aditya Prajapati <206730797+adityapraja@users.noreply.github.com>
2026-06-10 12:50:04 +0530

    kubernetes yamls fixed

---
+++ k8s/script.sh
@@ -0,0 +1,9 @@
+#script to run all resources at a time
+kubectl apply -f mongodb-secret.yaml
+kubectl apply -f mongodb-config.yaml
+kubectl apply -f mongodb-pvc.yaml
+kubectl apply -f app-configmap.yaml
+kubectl apply -f mongodb-deployment.yaml
+kubectl apply -f mongodb-service.yaml
+kubectl apply -f app-deployment.yaml
+kubectl apply -f app-service.yaml
