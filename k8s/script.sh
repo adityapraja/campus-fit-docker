44854d498dc6fc214f6fd1888c0da03ca2b7ba4e
Aditya Prajapati <206730797+adityapraja@users.noreply.github.com>
2026-06-11 15:45:10 +0530

    ingress added

--- k8s/script.sh
+++ k8s/script.sh
@@ -1,9 +1,11 @@
 #script to run all resources at a time
 kubectl apply -f mongodb-secret.yaml
-kubectl apply -f mongodb-config.yaml
+#kubectl apply -f mongodb-config.yaml
 kubectl apply -f mongodb-pvc.yaml
 kubectl apply -f app-configmap.yaml
 kubectl apply -f mongodb-deployment.yaml
 kubectl apply -f mongodb-service.yaml
 kubectl apply -f app-deployment.yaml
 kubectl apply -f app-service.yaml
+minikube addons enable ingress
+kubectl apply -f app-ingress.yaml
